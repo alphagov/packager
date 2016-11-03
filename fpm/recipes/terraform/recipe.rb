f81ee18ce75719623aaa2d55d01686baec5bc9bf
dazahern <daz.ahern@digital.cabinet-office.gov.uk>
2016-11-03 16:40:39 +0000

    Upgrade Terraform to 0.7.7
    Upgraded Terraform to 0.7.7 to keep it up to date.

--- fpm/recipes/terraform/recipe.rb
+++ fpm/recipes/terraform/recipe.rb
@@ -2,10 +2,10 @@ class Terraform < FPM::Cookery::Recipe
   name 'terraform'
   homepage 'https://www.terraform.io/'
 
-  version '0.7.0'
+  version '0.7.7'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
-  sha256 'a196c63b967967343f3ae9bb18ce324a18b27690e2d105e1f38c5a2d7c02038d'
+  sha256 '478c4fe311392804ffc449995e8d7f903abab56f7483f317c1f120d8c21b1a81'
 
   maintainer 'GOV.UK <govuk-dev@digital.cabinet-office.gov.uk>'
   license 'Mozilla Public License, version 2.0'
