fc83bc27fa9c4eb94f1e28469b168862080a41ff
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2016-11-07 13:34:40 +0000

    Upgraded Terraform to 0.7.9
    Upgraded Terraform to 0.7.9 to keep it up to date
    
    https://github.com/hashicorp/terraform/blob/master/CHANGELOG.md#079-november-4-2016

--- fpm/recipes/terraform/recipe.rb
+++ fpm/recipes/terraform/recipe.rb
@@ -2,10 +2,10 @@ class Terraform < FPM::Cookery::Recipe
   name 'terraform'
   homepage 'https://www.terraform.io/'
 
-  version '0.7.7'
+  version '0.7.9'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
-  sha256 '478c4fe311392804ffc449995e8d7f903abab56f7483f317c1f120d8c21b1a81'
+  sha256 'ac1d0302818ae17f1042dc26407e7ff94cd1e34ed260dae9d72c75a4d0b59cfc'
 
   maintainer 'GOV.UK <govuk-dev@digital.cabinet-office.gov.uk>'
   license 'Mozilla Public License, version 2.0'
