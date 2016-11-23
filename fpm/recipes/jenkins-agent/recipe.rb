253a36f1c0a7f36a6a4a210e75849319a8e4155e
Laura Martin <surminus@gmail.com>
2016-11-23 18:07:30 +0000

    Add jenkins-agent
    Create a package to download and install the Jenkins Swarm plugin aka
    jenkins-agent

---
+++ fpm/recipes/jenkins-agent/recipe.rb
@@ -0,0 +1,23 @@
+class JenkinsAgent < FPM::Cookery::Recipe
+  description 'The Jenkins swarm plugin enables slaves to auto-discover nearby Jenkins master and join it automatically, thereby forming an ad-hoc cluster.'
+  name 'jenkins-agent'
+  homepage 'https://jenkins.io/'
+
+  version '2.2'
+
+  source "https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/#{version}/swarm-client-#{version}-jar-with-dependencies.jar"
+  sha256 'fc5ad10aaca1a3563c1ec650ad9bcb8ecbea0be9cd10053421f89647daeef8eb'
+
+  maintainer 'GOV.UK <govuk-dev@digital.cabinet-office.gov.uk>'
+
+  def build
+  end
+
+  def install
+    package_name = "swarm-client-#{version}-jar-with-dependencies.jar"
+
+    safesystem "mkdir -p #{destdir}/usr/local/bin/"
+    safesystem "cp -f #{builddir}/#{package_name}/#{package_name} /usr/local/bin/jenkins-agent"
+  end
+
+end
