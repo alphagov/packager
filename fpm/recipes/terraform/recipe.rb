86d2df7ee2a45027caf6ff85c1dfd7427b87d1d4
dazahern <daz.ahern@digital.cabinet-office.gov.uk>
2016-12-15 15:18:17 +0000

    Upgrading Terraform to 0.8.1
    Upgrading Terraform again due to a whitespace bug in 0.8.0

--- fpm/recipes/terraform/recipe.rb
+++ fpm/recipes/terraform/recipe.rb
@@ -2,10 +2,10 @@ class Terraform < FPM::Cookery::Recipe
   name 'terraform'
   homepage 'https://www.terraform.io/'
 
-  version '0.8.0'
+  version '0.8.1'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
-  sha256 '9f99258396dad71746fb96036bf421e05fb3f36f402b89c3f16b453f3f0ee9cf'
+  sha256 'da98894a79b7e97ddcb2a1fed7700d3f53c3660f294fb709e1d52c9baaee5c59'
 
   maintainer 'GOV.UK <govuk-dev@digital.cabinet-office.gov.uk>'
   license 'Mozilla Public License, version 2.0'
