8c192c43733150a5727b1eec85bc8520f70d7bb2
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2019-06-18 16:47:59 +0100

    Upgrade the Terraform package to 0.11.14
    https://github.com/hashicorp/terraform/blob/v0.11/CHANGELOG.md#01114-may-16-2019

--- fpm/recipes/terraform/recipe.rb
+++ fpm/recipes/terraform/recipe.rb
@@ -2,10 +2,10 @@ class Terraform < FPM::Cookery::Recipe
   name 'terraform'
   homepage 'https://www.terraform.io/'
 
-  version '0.11.7'
+  version '0.11.14'
 
   source "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
-  sha256 '6b8ce67647a59b2a3f70199c304abca0ddec0e49fd060944c26f666298e23418'
+  sha256 '9b9a4492738c69077b079e595f5b2a9ef1bc4e8fb5596610f69a6f322a8af8dd'
 
   maintainer 'GOV.UK <govuk-dev@digital.cabinet-office.gov.uk>'
   license 'Mozilla Public License, version 2.0'
